808ed9312e4371a05cd41d2bfdaed926ab1c0b85
gmmowry <glennamowry@gmail.com>
2015-08-11 16:52:58 -0500

    added possible solution for basic methods

--- solutions/linked_lists_basic_methods_solutions_gmmowry.rb
+++ solutions/linked_lists_basic_methods_solutions_gmmowry.rb
@@ -1,4 +1,20 @@
+class Node
+  attr_accessor :value, :next
+  def initialize(value)
+    @value = value
+  end
+end
+
+
 class LinkedList
+  attr_accessor :head, :size
+
+  # initially, there is no 'head' and
+  # the size of the list is 0
+  def initialize
+    @head = nil
+    @size = 0
+  end
 
   def [](position)
     current_node = @head
@@ -32,3 +48,54 @@ class LinkedList
 end
 
 
+
+
+# Another way of solving 'insert' and 'remove'
+
+  # if index is zero, due bubble sorty thing
+  # to re-assign a new head. otherwise, loop
+  # through to the index before desired, set
+  # new_node.next to current.next, set current.next
+  # to new_node
+  def add_at_index(value, index)
+    bounds_check(index)
+    new_node = Node.new(value)
+
+    if index == 0
+      dummy_node = @head
+      @head = new_node
+      @head.next = dummy_node
+    else
+      current = @head
+      counter  = 0
+      while counter < index
+        if counter == index - 1
+          new_node.next = current.next
+          current.next = new_node
+        end
+        current = current.next
+        counter += 1
+      end
+    end
+    @size += 1
+  end
+
+  # if we remove the head, we simply assign
+  # head to head's reference. otherwise, set our current.next
+  # to current.next.next, skipping over the no longer
+  # needed node
+  def remove_at_index(index)
+    bounds_check(index)
+
+    if index == 0
+      @head = @head.next
+    else
+      current = @head
+      counter = 0
+      while counter < index
+        current.next = current.next.next || nil if counter == index - 1
+        counter += 1
+      end
+    end
+    @size -= 1
+  end
